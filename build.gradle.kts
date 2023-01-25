b1636a6f0f6c209e35efa564efa93aba1c431455
Nayan Hajratwala <nayan@chikli.com>
2023-01-24 19:56:23 -0500

    spruce up the gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,37 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "org.geepawhill"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 plugins {
     kotlin("jvm") version "1.7.22"
 }
 
 repositories {
-    maven {
-        setUrl("https://oss.sonatype.org/content/repositories/snapshots/")
-    }
     mavenCentral()
 }
 
 dependencies {
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
-    testImplementation("org.assertj:assertj-core:3.23.1")
     implementation("org.jsoup:jsoup:1.15.3")
+
+    testImplementation("org.assertj:assertj-core:3.23.1")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
+val jvmVersion: JavaLanguageVersion = JavaLanguageVersion.of(17)
 kotlin {
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = "17"
-        }
+    jvmToolchain(jvmVersion.asInt())
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = jvmVersion.toString()
     }
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
-
