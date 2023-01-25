39cfc729b03d7f8c106d8e805c396b28dd40b893
GeePaw Hill <geepawhill@geepawhill.org>
2023-01-25 15:46:15 -0500

    Tightened up BasicInternalTagTest.

--- src/test/kotlin/org/geepawhill/html/basic/BasicInternalTagTest.kt
+++ src/test/kotlin/org/geepawhill/html/basic/BasicInternalTagTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class BasicInternalTagTest {
 
     @Test
-    fun `nested tags work`() {
+    fun `nested tags with construction`() {
         val tag = BasicInternalTag("div").apply {
             +BasicInternalTag("li")
         }
@@ -27,11 +27,20 @@ class BasicInternalTagTest {
     }
 
     @Test
-    fun `nested texts work`() {
+    fun `nested texts construction`() {
         val tag = BasicInternalTag("div").apply {
             +"hello"
         }
         assertThat(tag.elements).hasSize(1)
         assertThat(tag.emit()).isEqualTo("<div>hello</div>")
+    }
+
+    @Test
+    fun `nested texts with encoding`() {
+        val tag = BasicInternalTag("div").apply {
+            -"&"
+        }
+        assertThat(tag.elements).hasSize(1)
+        assertThat(tag.emit()).isEqualTo("<div>%26</div>")
     }
 }
