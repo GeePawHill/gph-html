18aef864a9f8259355e42692cb645ae171f90c2e
Nayan Hajratwala <nayan@chikli.com>
2023-01-26 11:30:32 -0500

    added some publishing stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,18 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "org.geepawhill"
-version = "0.0.1"
-
 plugins {
     kotlin("jvm") version "1.8.0"
+
+    // publishing
+    `maven-publish`
+    id("io.github.gradle-nexus.publish-plugin") version "1.1.0"
 
     // Check for updates with ./gradlew dependencyUpdates
     id("com.github.ben-manes.versions") version "0.44.0"
 
     // Formatting & Style
     id("io.gitlab.arturbosch.detekt") version "1.22.0"
+
 }
 
 repositories {
@@ -41,5 +43,22 @@ tasks.withType<KotlinCompile> {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {  //only for users registered in Sonatype after 24 Feb 2021
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        }
+    }
 }
 
