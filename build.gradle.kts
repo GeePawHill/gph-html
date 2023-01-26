22970cd523e54a41c26e5309ac9b0edfbf43f83c
Nayan Hajratwala <nayan@chikli.com>
2023-01-26 13:50:41 -0500

    added signing key stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -97,5 +97,8 @@ nexusPublishing {
 }
 
 signing {
+    val signingKey: String? by project
+    val signingPassword: String? by project
+    useInMemoryPgpKeys(signingKey, signingPassword)
     sign(publishing.publications["mavenJava"])
 }
