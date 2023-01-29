6a3a25bb8f6d83a15977c2fdf4ad0c4969e98820
GeePaw Hill <geepawhill@geepawhill.org>
2023-01-29 14:21:10 -0500

    Moved EncodedTextElementTest away from emit to FlatPrinter, deleted PrettyHtmlEmitterTest.

--- src/test/kotlin/org/geepawhill/html/basic/EncodedTextElementTest.kt
+++ src/test/kotlin/org/geepawhill/html/basic/EncodedTextElementTest.kt
@@ -1,14 +1,17 @@
 package org.geepawhill.html.basic
 
 import org.assertj.core.api.Assertions.assertThat
+import org.geepawhill.html.builder.FlatPrinter
 import org.geepawhill.html.model.EncodedTextElement
 import org.junit.jupiter.api.Test
 
 class EncodedTextElementTest {
 
+    val printer = FlatPrinter()
+
     @Test
     fun `encodes its argument on emit`() {
         val element = EncodedTextElement("&")
-        assertThat(element.emit()).isEqualTo("%26")
+        assertThat(printer.print(element)).isEqualTo("%26")
     }
 }
