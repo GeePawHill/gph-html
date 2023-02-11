f4a34d5d8b60e4c70bbbcdd46cfa1aa97dffb0bc
GeePaw Hill <geepawhill@geepawhill.org>
2023-02-11 18:45:55 -0500

    Finished PrettyFormatter and tests for both formatters.

--- src/main/kotlin/org/geepawhill/html/formatter/PrettyFormatter.kt
+++ src/main/kotlin/org/geepawhill/html/formatter/PrettyFormatter.kt
@@ -4,56 +4,98 @@ import org.geepawhill.html.map.KeyAndValue
 import org.geepawhill.html.model.HtmlFormatter
 
 @Suppress("TooManyFunctions")
-class PrettyFormatter : HtmlFormatter {
+class PrettyFormatter(
+    private val appendable: Appendable = StringBuilder(),
+    val indentString: String = PrettyFormatter.DEFAULT_INDENT
+) : HtmlFormatter {
+
+    private var depth = 0
+
     override fun emptyTag(tag: String, attributes: Collection<KeyAndValue>) {
-        TODO("Not yet implemented")
+        newline()
+        appendable.append("<$tag")
+        for (attribute in attributes) appendable.append(" ${attribute.key}=\"${attribute.value}\"")
+        appendable.append(">")
     }
 
     override fun text(text: String) {
-        TODO("Not yet implemented")
+        appendable.append(text)
     }
 
     override fun openContainerTag(tag: String, attributes: Collection<KeyAndValue>) {
-        TODO("Not yet implemented")
+        emptyTag(tag, attributes)
+        depth += 1
     }
 
     override fun closeContainerTag(tag: String) {
-        TODO("Not yet implemented")
+        depth -= 1
+        newline()
+        appendable.append("</$tag>")
     }
 
     override fun openSelector(rule: String) {
-        TODO("Not yet implemented")
+        newline()
+        appendable.append("$rule {")
+        depth += 1
     }
 
     override fun closeSelector() {
-        TODO("Not yet implemented")
+        depth -= 1
+        newline()
+        appendable.append("}")
     }
 
     override fun declaration(declaration: KeyAndValue) {
-        TODO("Not yet implemented")
+        newline()
+        appendable.append("${declaration.key}: ${declaration.value};")
     }
 
     override fun startStyles() {
-        TODO("Not yet implemented")
+        newline()
+        appendable.append("<style>")
+        depth += 1
     }
 
     override fun endStyles() {
-        TODO("Not yet implemented")
+        depth -= 1
+        newline()
+        appendable.append("</style>")
     }
 
     override fun startRule(rule: String) {
-        TODO("Not yet implemented")
+        newline()
+        appendable.append("$rule {")
+        depth += 1
     }
 
     override fun endRule() {
-        TODO("Not yet implemented")
+        depth -= 1
+        newline()
+        appendable.append("}")
     }
 
     override fun startQuery(query: String) {
-        TODO("Not yet implemented")
+        newline()
+        appendable.append("@media $query {")
+        depth += 1
     }
 
     override fun endQuery() {
-        TODO("Not yet implemented")
+        depth -= 1
+        newline()
+        appendable.append("}")
+    }
+
+    override fun toString(): String {
+        return appendable.toString()
+    }
+
+    private fun newline() {
+        appendable.append("\n")
+        (1..depth).forEach { _ -> appendable.append(indentString) }
+    }
+
+    companion object {
+        const val DEFAULT_INDENT: String = "  "
     }
 }
--- src/test/kotlin/org/geepawhill/html/formatter/PrettyFormattterTest.kt
+++ src/test/kotlin/org/geepawhill/html/formatter/PrettyFormattterTest.kt
@@ -4,13 +4,13 @@ import org.assertj.core.api.Assertions.assertThat
 import org.geepawhill.html.map.KeyAndValue
 import org.junit.jupiter.api.Test
 
-class FlatFormatterTest {
-    val formatter = FlatFormatter()
+class PrettyFormattterTest {
+    val formatter = PrettyFormatter()
 
     @Test
     fun `handles empty tag`() {
         formatter.emptyTag("tag", listOf(KeyAndValue("a", "avalue"), KeyAndValue("b", "bvalue")))
-        assertThat(formatter.toString()).isEqualTo("<tag a=\"avalue\" b=\"bvalue\">")
+        assertThat(formatter.toString().trim()).isEqualTo("<tag a=\"avalue\" b=\"bvalue\">")
     }
 
     @Test
@@ -22,66 +22,75 @@ class FlatFormatterTest {
     @Test
     fun `handles open container tag`() {
         formatter.openContainerTag("tag", listOf(KeyAndValue("key", "value")))
-        assertThat(formatter.toString()).isEqualTo("<tag key=\"value\">")
+        assertThat(formatter.toString().trim()).isEqualTo("<tag key=\"value\">")
     }
 
     @Test
     fun `handles close container tag`() {
         formatter.closeContainerTag("tag")
-        assertThat(formatter.toString()).isEqualTo("</tag>")
+        assertThat(formatter.toString()).isEqualTo("\n</tag>")
+    }
+
+    @Test
+    fun `handles child tags`() {
+        formatter.openContainerTag("parent", emptyList())
+        formatter.emptyTag("child", emptyList())
+        formatter.closeContainerTag("parent")
+        val expected = "\n<parent>\n  <child>\n</parent>"
+        assertThat(formatter.toString()).isEqualTo(expected)
     }
 
     @Test
     fun `handles open selector`() {
         formatter.openSelector("rule")
-        assertThat(formatter.toString()).isEqualTo(" rule {")
+        assertThat(formatter.toString()).isEqualTo("\nrule {")
     }
 
     @Test
     fun `handles close selector`() {
         formatter.closeSelector()
-        assertThat(formatter.toString()).isEqualTo(" }")
+        assertThat(formatter.toString()).isEqualTo("\n}")
     }
 
     @Test
     fun `handles declaration`() {
         formatter.declaration(KeyAndValue("width", "100px"))
-        assertThat(formatter.toString()).isEqualTo(" width: 100px;")
+        assertThat(formatter.toString()).isEqualTo("\nwidth: 100px;")
     }
 
     @Test
     fun `handles start styles`() {
         formatter.startStyles()
-        assertThat(formatter.toString()).isEqualTo("<style>")
+        assertThat(formatter.toString()).isEqualTo("\n<style>")
     }
 
     @Test
     fun `handles end styles`() {
         formatter.endStyles()
-        assertThat(formatter.toString()).isEqualTo("</style>")
+        assertThat(formatter.toString()).isEqualTo("\n</style>")
     }
 
     @Test
     fun `handles start rule`() {
         formatter.startRule("*")
-        assertThat(formatter.toString()).isEqualTo("* {")
+        assertThat(formatter.toString()).isEqualTo("\n* {")
     }
 
     @Test
     fun `handles end rule`() {
         formatter.endRule()
-        assertThat(formatter.toString()).isEqualTo(" }")
+        assertThat(formatter.toString()).isEqualTo("\n}")
     }
 
     @Test
     fun `handles start media`() {
         formatter.startQuery("printer only")
-        assertThat(formatter.toString()).isEqualTo("@media printer only { ")
+        assertThat(formatter.toString()).isEqualTo("\n@media printer only {")
     }
 
     @Test
     fun `handles end media`() {
         formatter.endQuery()
-        assertThat(formatter.toString()).isEqualTo(" }")
+        assertThat(formatter.toString()).isEqualTo("\n}")
     }
 }
